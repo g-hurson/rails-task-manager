34ffd85438cfe6694541eeee8412b348496557a4
Geraldine Hurson <geraldinehurson@gmail.com>
2024-05-21 15:08:59 +0200

    --wip-- [skip ci]

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  # get "tasks", to: "tasks#index", as: :tasks
+  # get "tasks/new", to: "tasks#new"
+  # get "tasks/:id", to: "tasks#show", as: :task
+  # post "tasks", to: "tasks#create"
+  # get "tasks/:id/edit", to: "tasks#edit", as: :edit
+  # patch "tasks/:id", to: "tasks#update"
+  # delete "tasks/:id", to: "tasks#destroy"
+  resources :tasks
 end
